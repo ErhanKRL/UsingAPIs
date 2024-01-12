f6be61bdd36a1ec95a9666ef839781d7f16f35b7
Erhan Koral <erhan.16k@gmail.com>
2024-01-12 16:08:21 +0100

    week-2 exercise completed

--- Week2/prep-exercises/1-catwalk-async-await/index.js
+++ Week2/prep-exercises/1-catwalk-async-await/index.js
@@ -5,17 +5,26 @@ const STEP_SIZE_PX = 10;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
   'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL = "http://www.anniemation.com/clip_art/images/cat-walk.gif"
+  
+  function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+async function walk(img, startPos, stopPos) {
+    let currentPosition = startPos;
+    while (currentPosition < stopPos) {
+      currentPosition += STEP_SIZE_PX;
+      img.style.left = currentPosition + 'px';
+      await wait(STEP_INTERVAL_MS);
+    }
 
-function walk(img, startPos, stopPos) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
-  });
 }
 
-function dance(img) {
-  return new Promise((resolve) => {
-    // Copy over the implementation from last week
-  });
+async function dance(img) {
+    img.src = DANCING_CAT_URL;
+    await wait(DANCE_TIME_MS);
+    img.src = WALKING_CAT_URL;
+  
 }
 
 async function catWalk() {
@@ -24,7 +33,11 @@ async function catWalk() {
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
 
-  // Use async/await syntax to loop the walk and dance functions
+  while(true){
+    await walk(img, startPos, centerPos);
+    await dance(img);
+    await walk(img, centerPos, stopPos);
+  }
 }
 
 window.addEventListener('load', catWalk);
